f9425ba3081650f6b7f70e0a6e3f8af4106aa8d9
kotireddydeva <devakoti.fullstackdeveloper@gmail.com>
2025-07-04 15:40:59 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const servicesRoute = require('./routes/services');
 
+app.use(cors());
+app.use(express.json());
+app.use('/api/services', servicesRoute);
+
+app.listen(5000, () => console.log('🚀 Server running on http://localhost:5000'));
